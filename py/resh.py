53227dff866b2f7f4e2c8ad8a303d263d6c46d05
Visstudil <86619717+Visstudil@users.noreply.github.com>
2021-12-25 23:53:37 +0400

    refactoring

--- py/resh.py
+++ py/resh.py
@@ -7,87 +7,84 @@
 
 
 import requests
+from requests.sessions import Session
+import cache
 import json
 from bs4 import BeautifulSoup as bs
 from threading import Thread
 
 
 class Parser():
-
-    #Urls:
     MAIN_URL = "https://resh.edu.ru"
-    SUBJECT_SUFFIX = "/subject"
-    PAGE_REQUEST = lambda _subject, _class, _page: \
-        "%s/subject/%s/%s/ajax?page=%s" % (Parser.MAIN_URL,
-                                            _subject,
-                                            _class,
-                                            _page)
-    STRIP = lambda Url: int(str.strip(Url, "/subject/lesson/"))
-    LESSON_WRAP = lambda lesson: \
-        Parser.MAIN_URL + lesson + "train"
 
-    #Dictionaries:
+    #Dictionaries
     SUBJECTS = {}
     LESSONS = {}
     TASKS = {}
-
-    def __init__(self):
-        # Parse the school subjects page and write links to dictionary:
-        request = requests.get(Parser.MAIN_URL + Parser.SUBJECT_SUFFIX)
-        soup = bs(request.text, "html.parser")
-        for subjectCell in soup.find_all("a", "subject-cell"):
-            Parser.SUBJECTS[subjectCell.
-            select_one("span").string] = Parser.STRIP(subjectCell.get("href"))
-
-    def parse_lessons_links(self, _subject, _class):
-        pageNumber = 1
-        lessonNumber = 1
-
-        while pageNumber is not False:
-            request = requests.get(Parser.PAGE_REQUEST(_subject,
-                                                        _class,
-                                                        pageNumber))
-            returnedHtml = request.json()["html"]
-            soup = bs(returnedHtml, "html.parser")
-            for lessonBlock in soup.find_all("a", "lesson-block"):
-                self.LESSONS[lessonNumber] = Parser.LESSON_WRAP(lessonBlock["href"])
-                lessonNumber += 1
-            pageNumber = request.json()["nextPage"]
+    ANSWERS = {}
     
-    def parse_tasks_links(self, _lesson):
-        taskNumber = 1
+    #Lambdas
+    ANSWERS_REQUEST = lambda task: Parser.MAIN_URL + "/tests/%s/get-answers" % task
+    LESSON_HREF = lambda lesson: Parser.MAIN_URL + lesson + "train"
+    PAGE_REQUEST = lambda subject, grade, page: \
+        "%s/subject/%s/%s/ajax?page=%s" % (Parser.MAIN_URL,
+                                            subject,
+                                            grade,
+                                            page)
+    STRIP = lambda Url: int(str.strip(Url, "/subject/lesson/"))
+    
+    def parse_subjects(self):
+        # Parses all subjects
+        with requests.Session() as session:
+            request = session.post(Parser.MAIN_URL  + "/subject")
+            soup = bs(request.text, "html.parser")
+            for subjectCell in soup.find_all("a", "subject-cell"):
+                Parser.SUBJECTS[subjectCell.
+                select_one("span").string] = Parser.STRIP(subjectCell.get("href"))
 
-        request = requests.get(self.LESSONS[_lesson])
-        returnedHtml = request.text
-        soup = bs(returnedHtml, "html.parser")
-        for taskBlock in soup.find_all("li", "test__task-num"):
-            self.TASKS[taskNumber] = Parser.STRIP(taskBlock["data-test-id"])
-            taskNumber += 1
+    def parse_lessons(self, subject, grade):
+        # Parses all lessons
+        with requests.Session() as session:
+            page = 1
+            lesson = 1
+            while page is not False:
+                request = session.get(Parser.PAGE_REQUEST(subject, grade, page))
+                returnedHtml = request.json()["html"]
+                soup = bs(returnedHtml, "html.parser")
+                for lessonBlock in soup.find_all("a", "lesson-block"):
+                    self.LESSONS[lesson] = Parser.LESSON_HREF(lessonBlock["href"])
+                    lesson += 1
+                page = request.json()["nextPage"]
+    
+    def parse_tasks(self, lesson):
+        # Parses all tasks
+        with requests.Session() as session:
+            task = 1
 
-    def parse_answers(self):
-        pass
+            request = session.get(self.LESSONS[lesson])
+            returnedHtml = request.text
+            soup = bs(returnedHtml, "html.parser")
+            for taskBlock in soup.find_all("li", "test__task-num"):
+                self.TASKS[task] = Parser.STRIP(taskBlock["data-test-id"])
+                task += 1
 
-    def write_json(self):
-        pass
+    def parse_answers(self, subject, grade, lesson):
+        # Parses all answers
+        with requests.Session() as session:
+            for task in self.TASKS.values():
+                request = session.post(Parser.ANSWERS_REQUEST(task),
+                headers={"X-Requested-With": "XMLHttpRequest"})
+                self.ANSWERS[task] = request.iter_lines()[0]
 
-    def cache(self):
-        pass
 
-    def __del__(self):
-        pass
+class FileManager():
+    
+    def write_json(self, message, subject, grade, lesson, task):
+        with open("%s %s класс - урок №%s задание №%s.json" %
+        (subject, grade, lesson, task), 
+        "w", encoding="utf-8") as file:
+            json.dump(message, file, indent=4, ensure_ascii=False)
 
 
-parser = Parser()
-
-initialize = Thread(target=parser.__init__())
-initialize.start()
-print(parser.SUBJECTS)
-
-selectedSubject = str(input("subject >>>"))
-selectedClass = int(input("class >>>"))
-selectedLesson = int(input("lesson >>>"))
-
-parser.parse_lessons_links(Parser.SUBJECTS[selectedSubject], selectedClass)
-print(Parser.LESSONS)
-parser.parse_tasks_links(selectedLesson)
-print(Parser.TASKS)
+class CacheManager():
+    pass
